fa29c6449805dc19ca97e9ec377f2b22e0b574a0
garvv29 <garv.choure2023@ssipmt.com>
2026-06-25 12:28:02 +0530

    updated defaulthash to hash based on their addresses

--- tests/hashmap/test2.cpp
+++ tests/hashmap/test2.cpp
@@ -3,36 +3,23 @@
 
 using namespace std;
 
-
 int main()
 {
     struct stud{
         string name;
         string rollno;
         stud(string name,string rollno):name(name),rollno(rollno){}
-         bool operator==(const stud& other) const
+    bool operator==(const stud& other) const
     {
         return rollno == other.rollno;
     }
     };
-    struct StudHash
-{
-    size_t operator()(const stud& s) const
-    {
-        return DefaultHash<std::string>()(
-            s.rollno
-        );
-    }
-};
-    stud grv("grv","101");
-    stud gv("gv","102");
-    stud v("v","103");
-    stud g("g","104");
-    HashMap<stud,int,StudHash> garv;
-    garv.insert(g,1);
-    garv.insert(gv,1);
-    garv.insert(v,1);
-    garv.insert(grv,1);
-    garv.debugPrint();
-    cout<<garv.collisions();
+stud garv("g","1");
+stud gar("ga","2");
+stud ga("gar","2");
+HashMap<stud,int> map;
+map.insert(garv,1);
+map.insert(gar,2);
+map.insert(ga,2);
+map.debugPrint();
 }
